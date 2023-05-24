a1b7e06f9ea21b05df8ce478a81686461d7aa770
Alessandro Innocenti <alessandro.innocenti8@gmail.com>
2023-05-24 12:57:54 +0200

    Added categories

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,20 @@ from django.db import models
 
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts', null=True)
     title = models.CharField(max_length=200)
     intro = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Post
+from .models import Post, Category
 from .forms import CommentForm
 
 
 # Create your views here.
-def detail(request, slug):
+def detail(request, category_slug, slug):
     post = get_object_or_404(Post, slug=slug)
 
     if request.method == 'POST':
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post=post
+            comment.post = post
             comment.save()
 
             return redirect('post_detail', slug=slug)
@@ -22,3 +22,9 @@ def detail(request, slug):
     form = CommentForm()
 
     return render(request, 'blog/detail.html', {'post': post, 'form': form})
+
+
+def category(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+
+    return render(request, 'blog/category.html', {'category': category})
