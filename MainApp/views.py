69d85df3aad461d846f137f8f2142c112de61488
Alessandro Innocenti <alessandro.innocenti8@gmail.com>
2023-05-24 11:21:01 +0200

    Added post management in admin django

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+
 
 # Create your views here.
+
+def homepage(request):
+    return render(request, 'MainApp/home.html')
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    intro = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    body = models.TextField(max_length=800)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.title
