d4609ab857212fb009f3a226d90948604e7ff9d4
Alessandro Innocenti <alessandro.innocenti8@gmail.com>
2023-05-24 18:28:54 +0200

    Added sign up confirmation and other fixes

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 from .forms import CreateUserForm
 
 
@@ -13,9 +15,16 @@ def signup(request):
         form = CreateUserForm(request.POST)
         if form .is_valid():
             form.save()
+            messages.success(request, 'Account was created for ' + form.cleaned_data.get('username'))
+            return redirect('login')
 
     return render(request, 'account/signup.html', {'form': form})
 
 
 def login(request):
+
+    if request.method == 'POST':
+        request.POST.get('username')
+        request.POST.get('password')
+
     return render(request, 'account/login.html')
