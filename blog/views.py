f1d9e615dccf8455674cd06528cc78a393a64083
Alessandro Innocenti <alessandro.innocenti8@gmail.com>
2023-05-24 16:52:05 +0200

    Completed search page

--- blog/views.py
+++ blog/views.py
@@ -34,5 +34,5 @@ def category(request, slug):
 
 def search(request):
     query = request.GET.get('query', '')
-    posts = Post.objects.filter(Q(title__icontains=query) | Q(body__icontains=query) | Q(intro__icontains=query))
+    posts = Post.objects.filter(status=Post.ACTIVE).filter(Q(title__icontains=query) | Q(body__icontains=query) | Q(intro__icontains=query))
     return render(request, 'blog/search.html', {'posts': posts, 'query': query})
